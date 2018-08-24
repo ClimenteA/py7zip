0bf9ad6007b54666b9a9954f840b37424f2188bd
Climente Alin <aclimente@ymail.com>
2018-08-24 13:54:00 +0300

    Add files via upload

--- unzip.py
+++ unzip.py
@@ -1,18 +1,11 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
-import re, os
+import re, os, sys
 import subprocess
 from zipfile import ZipFile
 import pandas as pd
 
 
 
-
-print("\n\nMake sure you have zipPassword.xlsx next to the program\n")
+print("\n\nMake sure you have zipPassword.xlsx next to the program and you have sufficient space on disk\n")
 
 
 zippedPath = input("\n\nInsert the path to 'zipped files' below:\n")
@@ -51,8 +44,12 @@ def execute7z(zipPathInput, zipPassword, zipPathOutput):
     """Excecute 7zip batch command using subprocess call
        x {} - full path to zip; -p"{}" - password; -o{} - output path of unzip
     """#'7z x {} -p"{}" -o{}'
-    subprocess.Popen([r"7z/7za.exe", "x", zipPathInput, "-p{}".format(zipPassword), "-o{}".format(zipPathOutput)])
+    process = subprocess.Popen([r"7z/7za.exe", "x", zipPathInput, "-p{}".format(zipPassword), "-o{}".format(zipPathOutput)])
+    process.wait()
+    process.kill()
+    return 0
 
+                                    
 
 def zipFilesNames(zipName):  
     """Get a list of names inside that zip files"""
@@ -82,28 +79,122 @@ def nestedYear(zipFilePath):
             return year
 
 
+
+
+def getDirSize(dirPath):
+    """Get a directory size"""
+    total_size = 0
+    for path, dirs, files in os.walk(dirPath):
+        for f in files:
+            fp = os.path.join(path, f)
+            total_size += os.path.getsize(fp)
+    return int(total_size)
+
+def getfileSize(filepath):
+    """Get file size"""
+    metadata = os.stat(filepath)
+    file_size = int(metadata.st_size) # bytes
+    
+    return file_size
+
+def getSize(apath):
+    """Get file or dir size"""
+    if os.path.isfile(apath):
+        return getfileSize(apath)
+    elif os.path.isdir(apath):
+        return getDirSize(apath)
+    else:
+        raise ValueError("Input is neither a path or a file!")
+
+
+def checkZIP(zipFilePath, unzippedFilePath):
+    """Check if zip extracted corectly by verifying zip and unzipped size"""
+    zip_size = getSize(zipFilePath)
+    unzip_size = getSize(unzippedFilePath)
+     
+    #print("zip_size = {}, unzip_size = {}".format(zip_size, unzip_size))
+    
+    if unzip_size < zip_size/2:
+        return False
+    else:
+        return True
+        
+
+
 try:
     print("\n\nPlease wait...\n")
     createUnzipDirs(unzip_files_paths)
+    
+    try:
+        os.remove("errors.txt")
+    except:
+        pass
+
+    err = open("errors.txt", "a")
+
 
     passdf = pd.read_excel("zipPassword.xlsx")
     passdf_idx = passdf.index.tolist()
 
-    for i in passdf_idx:
-        ac = passdf.loc[i, "A/C"]
-        year = passdf.loc[i, "Year"]
-        password = passdf.loc[i, "Password"]
+    nokli = []
+    for j, zfp in enumerate(zip_files_paths):
+        zfName = zfp.split("\\")[-1]
+        nestedYear_found = nestedYear(zfp)
+        
+        for i in passdf_idx:
+            try:
+                ac = passdf.loc[i, "A/C"]
+                year = passdf.loc[i, "Year"]
+                password = passdf.loc[i, "Password"]
 
-        for j, zfp in enumerate(zip_files_paths):
-            zfName = zfp.split("\\")[-1]
-            nestedYear_found = nestedYear(zfp)
-            if re.search(str(ac), zfName) and str(year) == str(nestedYear_found):
-                execute7z(zfp, password, unzip_files_paths[j])
-    
-    #print("\n\nDone!\n For any issue contact AtexisRO: alin.climente@atexis.eu")
-    #input("Press enter to exit..")
-    input()
+                if re.search(str(ac), zfName) and str(year) == str(nestedYear_found):
+                    process = execute7z(zfp, password, unzip_files_paths[j])
+                    if process == 0:
+                        if checkZIP(zfp, unzip_files_paths[j]):
+                            print("\nCool unzipping it's ok!\n")
+                            msg = "Unzipping ok for > {}\n".format(zfp)
+                            err.write(msg)
+                            #idxli.append(i)
+                        else:
+                            print("\nPlease check zip > ", zfp)
+                            errMsg = str("\nWarning: Got a big diference in sizes!\nPlease check zip > {} \nif it was unzipped corectly in > {}\n\n".format(zfp, unzip_files_paths[j]))
+                            err.write(errMsg)
+                            nokli.append(str("\nPlease check: " + str(zfp) + ' and ' + str(unzip_files_paths[j])))
+            except:
+                try:#not tested
+                    zipName = passdf.loc[i, "Name"]
+                    password = passdf.loc[i, "Password"]
+                except:
+                    zipName = zfName
+                    password = ''
+
+                if zipName == zfName:
+                    process = execute7z(zfp, password, unzip_files_paths[j])
+                    if process == 0:
+                        if checkZIP(zfp, unzip_files_paths[j]):
+                            print("\nCool unzipping it's ok!\n")
+                            msg = "\nUnzipping ok for > {}\n".format(zfp)
+                            err.write(msg)
+                            #idxli.append(i)
+                        else:
+                            print("\nPlease check zip > ", zfp)
+                            errMsg = str("\nWarning: Got a big diference in sizes!\nPlease check zip > {} \nif it was unzipped corectly in > {}\n\n".format(zfp, unzip_files_paths[j]))
+                            err.write(errMsg)
+                            nokli.append(str("\nPlease check: " + str(zfp) + ' and ' + str(unzip_files_paths[j])))
+
+
+    for n in nokli:
+        print(n)
+
+    err.close()
+    print("\n\nDone!\nFor any issue contact AtexisRO: alin.climente@atexis.eu")
+    input("Press enter to exit..")
 except Exception as e:
-    #print("Got this error: ", e)
-    #input("Press enter to exit..")
-    input()
+    err.close()
+    print("Got this error: ", e)
+    input("Press enter to exit..")
+
+
+
+
+
