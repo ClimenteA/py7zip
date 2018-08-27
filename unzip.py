2cdc281d00b38208f7273758619a39e903ef8203
Climente Alin <aclimente@ymail.com>
2018-08-27 16:22:47 +0300

    Add files via upload

--- unzip.py
+++ unzip.py
@@ -1,84 +1,27 @@
 import re, os, sys
 import subprocess
+import shutil
 from zipfile import ZipFile
 import pandas as pd
+import numpy as np
 
 
 
-print("\n\nMake sure you have zipPassword.xlsx next to the program and you have sufficient space on disk\n")
+def get_paths(zippedPath):
+    """Construct paths to zipped files and unzipped files"""
+    
+    unzippedPath = zippedPath.replace("zipped files", "unzipped files")
 
+    files = os.listdir(zippedPath)
+    zip_files = [z for z in files if re.search(".zip", z)] 
 
-zippedPath = input("\n\nInsert the path to 'zipped files' below:\n")
+    if len(files) != len(zip_files):
+        print("\nWarning you have files that are not in .zip format!")
 
-#zippedPath = r"E:\_python_macro\Unzip zip with pass\zipped files"
-unzippedPath = zippedPath.replace("zipped files", "unzipped files")
-
-
-months = ['JANUARY',
-          'FEBRUARY',
-          'MARCH',
-          'APRIL',
-          'MAY',
-          'JUNE',
-          'JULY',
-          'AUGUST',
-          'SEPTEMBER',
-          'OCTOBER',
-          'NOVEMBER',
-          'DECEMBER']
-
-files = os.listdir(zippedPath)
-zip_files = [z for z in files if re.search(".zip", z)] 
-zip_files_paths = [os.path.join(zippedPath, zp) for zp in zip_files]
-unzip_files_paths = [z.replace("zipped files", "unzipped files").replace(".zip", "").replace("Downloaded_documents_", "") for z in zip_files_paths]
-
-
-def createUnzipDirs(unzip_files_paths):
-    os.mkdir(unzippedPath)
-    for dirName in unzip_files_paths:
-        os.mkdir(dirName)
-
-
-
-def execute7z(zipPathInput, zipPassword, zipPathOutput):
-    """Excecute 7zip batch command using subprocess call
-       x {} - full path to zip; -p"{}" - password; -o{} - output path of unzip
-    """#'7z x {} -p"{}" -o{}'
-    process = subprocess.Popen([r"7z/7za.exe", "x", zipPathInput, "-p{}".format(zipPassword), "-o{}".format(zipPathOutput)])
-    process.wait()
-    process.kill()
-    return 0
-
-                                    
-
-def zipFilesNames(zipName):  
-    """Get a list of names inside that zip files"""
-    file = ZipFile(zipName)
-    filenames = []
-    for name in file.namelist():
-        filenames.append(name)
-    return filenames
-
-
-def getYear(zipName):
-    """Search for a month year pattern if found return year"""
-    for month in months:
-        pattern = month + " " +"\\d{4}"
-        match = re.search(pattern, zipName)
-        if match:
-            year = str(match.group().split(' ')[-1].strip())
-            return year
-
-
-def nestedYear(zipFilePath):
-    """Use zipFilesNames and getYear to return the year from the nested zip file"""
-    filesName = zipFilesNames(zipFilePath)
-    for fn in filesName:
-        year = getYear(fn)
-        if isinstance(year, str):
-            return year
-
-
+    zip_files_paths = [os.path.join(zippedPath, zp) for zp in zip_files]
+    unzip_files_paths = [z.replace("zipped files", "unzipped files").replace(".zip", "") for z in zip_files_paths]
+    
+    return unzippedPath, unzip_files_paths, zip_files_paths
 
 
 def getDirSize(dirPath):
@@ -90,12 +33,14 @@ def getDirSize(dirPath):
             total_size += os.path.getsize(fp)
     return int(total_size)
 
+
 def getfileSize(filepath):
     """Get file size"""
     metadata = os.stat(filepath)
     file_size = int(metadata.st_size) # bytes
     
     return file_size
+
 
 def getSize(apath):
     """Get file or dir size"""
@@ -105,6 +50,186 @@ def getSize(apath):
         return getDirSize(apath)
     else:
         raise ValueError("Input is neither a path or a file!")
+
+
+def zipFilesNames(zipName):  
+    """Get a list of names inside that zip files"""
+    file = ZipFile(zipName)
+    filenames = []
+    for name in file.namelist():
+        filenames.append(name)
+    file.close()
+    
+    return filenames
+
+
+def getZipInfo(zip_files_paths):
+    """Get from folder that contains zipped files the size and dir tree for each zip """
+    zipInfo = {}
+    for zfp in zip_files_paths:
+        #print("\nGet size/tree from:", zfp)
+        zipName = zfp.split('\\')[-1]
+        info = {'path': zfp, 'size': getSize(zfp), 'tree':zipFilesNames(zfp)}
+        zipInfo[zipName] = info
+    return zipInfo
+
+
+def dfZipInfo(zip_files_paths):    
+    """Create a df with the information from the zipInfo dict"""
+    
+    zipInfo = getZipInfo(zip_files_paths)
+
+    zname = []
+    zsize = []
+    ztree = []
+    zpath = []
+    for zipName, zipDict in zipInfo.items():
+        size = zipInfo[zipName]['size']
+        tree = zipInfo[zipName]['tree']
+        path = zipInfo[zipName]['path']
+
+        zname.append(zipName)
+        zsize.append(size)
+        ztree.append(" ".join(sorted(tree)))
+        zpath.append(path)
+
+    df = pd.DataFrame({})
+    df['zipName'] = pd.Series(zname)
+    df['zipPass'] = np.nan
+    df['zipPath'] = pd.Series(zpath)
+    df['zipSize'] = pd.Series(zsize)
+    df['zipTree'] = pd.Series(ztree)
+    df['zipDups'] = pd.Series(df.index.tolist())
+    
+    return df
+
+
+def solve_zip_duplicates(zip_files_paths):
+    """Filter from the df the zip files that are duplicates"""
+    
+    zipInfodf = dfZipInfo(zip_files_paths)
+
+    sizeGroup = zipInfodf.groupby(['zipSize'])
+    treeGroup = zipInfodf.groupby(['zipTree'])
+
+    for size, sdf in sizeGroup:
+        sdf_idxli = sdf.index.tolist()
+        if len(sdf_idxli) > 1:
+            #print(sdf_idxli)
+            for i in sdf_idxli:
+                zipInfodf.loc[i, 'zipDups'] = 'dups {}'.format(str(sdf_idxli))
+                zName = zipInfodf.loc[i, 'zipName']
+
+    for tree, tdf in treeGroup:
+        tdf_idxli = tdf.index.tolist()
+        if len(tdf_idxli) > 1:
+            #print(tdf_idxli)
+            for i in tdf_idxli:
+                zipInfodf.loc[i, 'zipDups'] = 'dups {}'.format(str(tdf_idxli))
+                zName = zipInfodf.loc[i, 'zipName']
+
+
+    zipInfodf.drop_duplicates(subset='zipDups', keep='last', inplace=True)
+    zipInfodf.reset_index(drop=True, inplace=True)
+
+    return zipInfodf
+
+
+def get_passwords():
+    """Open and get passwords in the excel next to executable. Make sure that is has column Password"""
+    cwd = os.getcwd()
+    files = os.listdir(cwd)
+    xlfile = [f for f in files if f == 'passwords.xlsx']
+    if len(xlfile) == 1:
+        passdf = pd.read_excel(xlfile[0])
+        if 'Passwords' not in passdf.columns.tolist():
+            raise Exception("The .xlsx file must have a column named Pasword")
+        return passdf['Passwords'].tolist()
+    else:
+        raise Exception("No '.xlsx' file found next to the executable!")
+
+
+def deletetree(apath):
+    """Delete folder and all of it's contents"""
+    try:
+        shutil.rmtree(apath) 
+        return True
+    except:
+        shutil.rmtree(apath, ignore_errors=True) #delete files that are not opened
+        response = "Could not remove folder {}, because some files are opened! Please delete them manually...".format(apath)
+    return response
+
+def createDir_bin():
+    """Create bin folder next to executable"""
+    try:
+        os.mkdir("bin")
+    except:
+        pass
+
+
+
+
+def checkPassforZip(zip_file_path, passwordsli, re_search_criteria=".pdf"):
+    """Check for a zip file what password works by extracting one file"""
+    zip_file_path = r"{}".format(zip_file_path)
+    files = zipFilesNames(zip_file_path)
+    file = [f for f in files if re.search(re_search_criteria, f)]
+    if len(file) != 0:
+        file = file[0]
+    
+    #zip_file_path = r"E:\_python_macro\Unzip zip with pass\zipped files\Downloaded_documents_B-HLA_17_08_2018_12_12_00.zip"
+    #p = "F367718:33:28"
+
+    for password in passwordsli:
+        pstr = "{}".format(str(password).strip())
+        createDir_bin()
+        try:
+            
+            with ZipFile(zip_file_path, 'r') as z:
+                z.extract(file, path="bin", pwd=bytes(pstr.encode()))
+                
+            deletetree('bin')
+            return password.strip()
+        
+        except Exception as e:
+            
+            if re.search('Bad password for file', str(e)):
+                #print("Bad password for zip file!")
+                deletetree('bin')
+                z.close()
+            else:
+                errmsg = "{}".format(str(e))
+                deletetree('bin')
+                z.close()
+                raise Exception(errmsg)
+
+
+
+def fillPasswords(zipdf):
+    """Fill df with passwords for each zip if found"""
+    
+    passwordsli = get_passwords()
+    
+    zfp_with_errors = []
+    for i, zfp in enumerate(zipdf['zipPath'].tolist()):
+        print("\nSearching password for: ", zfp)
+        try:
+            password = checkPassforZip(zfp, passwordsli)
+            if isinstance(password, str):
+                zipdf.loc[i, 'zipPass'] = password
+            else:
+                zipdf.loc[i, 'zipPass'] = 'Password not found'
+                zfp_with_errors.append(zfp)
+        except Exception as e:
+            print("Got: {} for: {}\n".format(str(e), zfp))
+            zipdf.loc[i, 'zipPass'] = 'Password not found'
+            zfp_with_errors.append(zfp)
+
+    print("\n\nCan't find passwords for these files:")
+    for zp in zfp_with_errors:
+        print('\n',zp)
+
+    zipdf.to_excel("zipInfo.xlsx", index=False)
 
 
 def checkZIP(zipFilePath, unzippedFilePath):
@@ -118,32 +243,24 @@ def checkZIP(zipFilePath, unzippedFilePath):
         return False
     else:
         return True
-        
 
 
-def checkPassforZip(zip_file_path, passwordsli, re_search_criteria=".pdf"):
-    """Check for a zip file what password works by extracting one file"""
-    files = zipFilesNames(zip_file_path)
-    file = [f for f in files if re.search(re_search_criteria, f)][-1]
-    
-    with ZipFile(zip_file_path, 'r') as z:
-        for p in passwordsli:
-            try:
-                pstr = "{}".format(p)
-                z.extract(file, path="/bin", pwd=bytes(pstr.encode()))
-                print("Password found: ", p)
-                return p
-            except RuntimeError:
-                #print("Password nok..")
-                pass
+def execute7z(zipPathInput, zipPassword, zipPathOutput):
+    """Excecute 7zip batch command using subprocess call
+       x {} - full path to zip; -p"{}" - password; -o{} - output path of unzip
+    """#'7z x {} -p"{}" -o{}'
+    process = subprocess.Popen([r"7z/7za.exe", "x", zipPathInput, "-p{}".format(zipPassword), "-o{}".format(zipPathOutput)])
+    process.wait()
+    process.kill()
+    return 0
 
 
 
+def masterUnzip(unzippedPath):
+    zipdf = pd.read_excel("zipInfo.xlsx")
+    zipdf = zipdf[zipdf["zipPass"] != 'Password not found']
+    zipdf.reset_index(drop=True, inplace=True)
 
-try:
-    print("\n\nPlease wait...\n")
-    createUnzipDirs(unzip_files_paths)
-    
     try:
         os.remove("errors.txt")
     except:
@@ -151,78 +268,75 @@ try:
 
     err = open("errors.txt", "a")
 
+    try:
+        os.mkdir(unzippedPath)
+    except:
+        print("Please delete or move 'unzipped files' folder!")
+        input("Press enter to exit...")
+        sys.exit()
 
-    passdf = pd.read_excel("zipPassword.xlsx")
-    passwordslist = passdf["Password"].tolist()
-    passdf_idx = passdf.index.tolist()
+    for i, zfp in enumerate(zipdf["zipPath"].tolist()):
+        unzip_file_path = zfp.replace("zipped files", "unzipped files")
+        password = zipdf.loc[i, "zipPass"]
 
-    nokli = []
-    for j, zfp in enumerate(zip_files_paths):
-        zfName = zfp.split("\\")[-1]
-        nestedYear_found = nestedYear(zfp)
-        
-        for i in passdf_idx:
-            try:
-                ac = passdf.loc[i, "A/C"]
-                year = passdf.loc[i, "Year"]
-                password = passdf.loc[i, "Password"]
+        os.mkdir(unzip_file_path)
 
-                if re.search(str(ac), zfName) and str(year) == str(nestedYear_found):
-                    if not isinstance(checkPassforZip(zfp, [password]), str):
-                        password = checkPassforZip(zfp, passwordslist)
-                        if not isinstance(password, str):
-                            msg = "\n\nPassword not found for > {}\n".format(zfp)
-                            err.write(msg)
-                            continue
+        process = execute7z(zfp, password, unzip_file_path)
 
-                    process = execute7z(zfp, password, unzip_files_paths[j])
-
-                    if process == 0:
-                        if checkZIP(zfp, unzip_files_paths[j]):
-                            print("\nCool unzipping it's ok!\n")
-                            msg = "Unzipping ok for > {}\n".format(zfp)
-                            err.write(msg)
-                            #idxli.append(i)
-                        else:
-                            print("\nPlease check zip > ", zfp)
-                            errMsg = str("\nWarning: Got a big diference in sizes!\nPlease check zip > {} \nif it was unzipped corectly in > {}\n\n".format(zfp, unzip_files_paths[j]))
-                            err.write(errMsg)
-                            nokli.append(str("\nPlease check: " + str(zfp) + ' and ' + str(unzip_files_paths[j])))
-            except:
-                try:#not tested
-                    zipName = passdf.loc[i, "Name"]
-                    password = passdf.loc[i, "Password"]
-                except:
-                    zipName = zfName
-                    password = ''
-
-                if zipName == zfName:
-                    process = execute7z(zfp, password, unzip_files_paths[j])
-                    if process == 0:
-                        if checkZIP(zfp, unzip_files_paths[j]):
-                            print("\nCool unzipping it's ok!\n")
-                            msg = "\nUnzipping ok for > {}\n".format(zfp)
-                            err.write(msg)
-                            #idxli.append(i)
-                        else:
-                            print("\nPlease check zip > ", zfp)
-                            errMsg = str("\nWarning: Got a big diference in sizes!\nPlease check zip > {} \nif it was unzipped corectly in > {}\n\n".format(zfp, unzip_files_paths[j]))
-                            err.write(errMsg)
-                            nokli.append(str("\nPlease check: " + str(zfp) + ' and ' + str(unzip_files_paths[j])))
-
-
-    for n in nokli:
-        print(n)
+        if process == 0:
+            if checkZIP(zfp, unzip_file_path):
+                print("\nCool unzipping it's ok!\n")
+                msg = "Unzipping ok for > {}\n".format(zfp)
+                err.write(msg)
+                #idxli.append(i)
+            else:
+                print("\nPlease check zip > ", zfp)
+                errMsg = str("\nWarning: Got a big diference in sizes!\nPlease check zip > {} \nif it was unzipped corectly in > {}\n\n".format(zfp, unzip_file_path))
+                err.write(errMsg)
 
     err.close()
-    print("\n\nDone!\nFor any issue contact AtexisRO: alin.climente@atexis.eu")
-    input("Press enter to exit..")
+
+
+
+##################################
+############# START #############
+##################################
+
+
+
+print("\n\nMake sure you have zipPassword.xlsx next to the program and you have sufficient space on disk\n")
+
+
+zippedPath = input("\n\nInsert the path to 'zipped files' below:\n")
+#zippedPath = r"E:\_python_macro\Unzip zip with pass\zipped files"
+try:
+    unzippedPath, unzip_files_paths, zip_files_paths = get_paths(zippedPath)
 except Exception as e:
-    err.close()
-    print("Got this error: ", e)
-    input("Press enter to exit..")
+    print("Path inserted can't be processed! Got: ", e)
+    input("Press enter to exit...")
+    sys.exit()
 
+skip = input("\nMake sure you have 'zipInfo.xlsx' filed corectly if you skip!\nSkip checks for duplicates and passwords checks?(y/n)\n")
 
-
-
-
+if skip.lower() == "n" or skip.lower() == "":
+    print("\nPlease wait..")
+    zipdf = solve_zip_duplicates(zip_files_paths)
+    fillPasswords(zipdf)
+    try:
+        masterUnzip(unzippedPath)
+        print("\n\nDone!\nFor any issue contact AtexisRO: alin.climente@atexis.eu")
+        input("Press enter to exit..")
+    except Exception as e:
+        print("\n\nGot this error: ", e)
+        input("Press enter to exit...")
+elif skip.lower() == "y":
+    print("\nPlease wait..")
+    try:
+        masterUnzip(unzippedPath)
+        print("\n\nDone!\nFor any issue contact AtexisRO: alin.climente@atexis.eu")
+        input("Press enter to exit..")
+    except Exception as e:
+        print("\n\nGot this error: ", e)
+        input("Press enter to exit...")
+else:
+    print("You can insert only y - yes and n - for no ")
